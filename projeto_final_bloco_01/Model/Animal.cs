46f0a5dcda2681c05b78ffe1f0775c1a780027d7
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-09-11 15:05:22 -0300

    model_repository

---
+++ projeto_final_bloco_01/Model/Animal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_final_bloco_01.Model
+{
+    public abstract class Animal
+    {
+        private int id;
+        private string nome;
+        private char genero;
+        private int idade;
+
+        protected Animal(int id, string nome, char genero, int idade)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.genero = genero;
+            this.idade = idade;
+        }
+
+        public int GetId() { return id; }
+        public string GetNome() { return nome; }
+        public char GetGenero() { return genero; }
+        public int GetIdade() { return idade; }
+
+        public void SetId(int id) { this.id = id; }
+        public void SetNome(string nome) { this.nome = nome; }
+        public void SetGenero(char genero) { this.genero = genero; }
+        public void SetIdade(int idade) { this.idade = idade; }
+
+        public abstract void Nadar();
+        public abstract void Correr();
+
+        public virtual void Visualizar()
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Dados da capivara");
+            Console.WriteLine("**************");
+            Console.WriteLine($"Identificação: {this.id}");
+            Console.WriteLine($"Nome da capivara: {this.nome}");
+            Console.WriteLine($"Genero da capivara: {this.genero}");
+            Console.WriteLine($"Idade da capivara:{this.idade}");
+        }
+    }
+
+}
---
+++ projeto_final_bloco_01/Model/Capivara.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_final_bloco_01.Model
+{
+    internal class Capivara : Animal
+    {
+        private int deh;
+        public Capivara(int id, string nome, char genero, int idade, int deh) : base(id, nome, genero, idade)
+        {
+            this.deh = deh;
+        }
+
+        public int GetDeh() { return deh; }
+        public void SetDeh(int deh) { this.deh = deh; }
+
+        public override void Correr()
+        {
+            Console.WriteLine("Estou correndo...");
+        }
+
+        public override void Nadar()
+        {
+            Console.WriteLine("Estou nadando...");
+        }
+    }
+}
